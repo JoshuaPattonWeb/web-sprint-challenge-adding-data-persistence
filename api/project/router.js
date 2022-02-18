35ffc2308195debb1b6af9bd1c3de7a46c3b9ed3
Joshua Patton <joshuapattonweb@gmail.com>
2022-02-18 14:35:14 -0500

    project model and router created

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,29 @@
 // build your `/api/projects` router here
+const { create } = require('domain')
+const express = require('express')
+const Projects = require('./model')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    Projects.getAll()
+        .then((project) => {
+        res.status(200).json(project)
+        })
+        .catch(next)
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const createProject = await Projects.create(req.body)
+        res.status(201).json({
+            project_id: createProject.project_id,
+            project_name: createProject.project_name,
+            project_description: createProject.project_description,
+            project_completed: createProject.project_completed === 0 ? false : true
+        })
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,14 +1,21 @@
 // build your server here and require it from index.js
 const express = require('express')
-
-
+const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
+const taskRouter = require('./task/router')
 
 const server = express()
 
 server.use(express.json())
+server.use('api/project', projectRouter)
+server.use('api/resource', resourceRouter)
+server.use('api/task', taskRouter)
 
-server.use('api/project', * )
-server.use('api/resource', * )
-server.use('api/task', * )
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
 
 module.exports = server
