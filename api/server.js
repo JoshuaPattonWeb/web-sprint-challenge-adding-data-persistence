2480ef973af37a2888f812f47ba85807a90249d6
Joshua Patton <joshuapattonweb@gmail.com>
2022-02-18 13:59:10 -0500

    depens, index template, server.js created

--- api/server.js
+++ api/server.js
@@ -1,1 +1,14 @@
 // build your server here and require it from index.js
+const express = require('express')
+
+
+
+const server = express()
+
+server.use(express.json())
+
+server.use('api/project', * )
+server.use('api/resource', * )
+server.use('api/task', * )
+
+module.exports = server
